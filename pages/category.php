185dde1aa8f91d7e833671ae55e7266f2bcb6ded
cheat2001 <chansocheatsok2001@gmail.com>
2023-03-03 18:13:27 +0700

    edit image in category

--- pages/category.php
+++ pages/category.php
@@ -69,7 +69,7 @@
         <div class="book-card">
           <div class="book-cover">
            <a href="./book-detail.php?id=<?php echo $row['id']; ?>">
-           <img src="http://localhost/book-store-project/images/<?php echo $row['image'] ?>" alt="<?php echo $row['image'] ?>">
+           <img src="../../book-store-project/images/<?php echo $row['image'] ?>" alt="<?php echo $row['image'] ?>">
            </a>
           </div>
           <div class="book-name">
