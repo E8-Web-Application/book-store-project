a9af6c475d9a6d869bc6a9d02f8492ecdbe9ae39
cheat2001 <chansocheatsok2001@gmail.com>
2023-03-16 00:29:12 +0700

    hashai

--- pages/account.php
+++ pages/account.php
@@ -1,7 +1,7 @@
 <?php session_start(); ?>
 <?php include("../partials/header.php"); ?>
 <?php include("../partials/navbar.php") ?>
-<?php include("../../book-store-project/partials/connect.php"); ?>
+<?php include("../../../book-store-project/partials/connect.php");?>
 <?php 
 $user_id=$_SESSION['user_id'];
 $sql ="SELECT image from user where id='$user_id';";
