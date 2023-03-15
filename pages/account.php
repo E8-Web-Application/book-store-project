e34fd5d33b2e106efde844579bf9872e7a216252
cheat2001 <chansocheatsok2001@gmail.com>
2023-03-16 00:35:25 +0700

    what

--- pages/account.php
+++ pages/account.php
@@ -3,10 +3,10 @@
 <?php include("../partials/navbar.php") ?>
 <?php include("../../../book-store-project/partials/connect.php");?>
 <?php 
-// $user_id=$_SESSION['user_id'];
-// $sql ="SELECT image from user where id='$user_id';";
-// $result=mysqli_query($conn,$sql);
-// $row=mysqli_fetch_assoc($result);
-// echo $row['']
+$user_id=$_SESSION['user_id'];
+$sql ="SELECT image from user where id='$user_id';";
+$result=mysqli_query($conn,$sql);
+$row=mysqli_fetch_assoc($result);
+echo $row['image'];
 ?>
 <?php include("../partials/footer.php"); ?>
